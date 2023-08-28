4d9a0128c848f0317675316002948fac1d157486
jamsumini <136440127+jamsumini@users.noreply.github.com>
2023-08-28 22:42:24 +0900

    CH4(3)1~N합구하기심화

---
+++ 2023/1~N합구하기심화.py
@@ -0,0 +1,7 @@
+plus=0
+print("1부터 N까지 더하기")
+N=int(input("N ?")) #하..괄호 닫혔는지 잘 확인해야되는군
+for i in range(1, N+1):
+    plus=plus+i
+    print("합계: ", plus)
+    #하 코드 보고 이해는 되는데 내가 입력하려면 너무어렵다 아직 이해를 못한듯
